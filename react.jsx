2f0cccb8b5c0d4c2cd806603fbddd1e993e9cfc6
zaidoi <zaidbadgujar7@gmail.com>
2025-11-14 23:48:48 +0530

    Working on React Chisps Input Question

--- react.jsx
+++ react.jsx
@@ -1,20 +1,42 @@
-Array.prototype.myReduce = function (callback, initialValue) {
-  if (typeof callback !== 'function') {
-      throw new TypeError("Callback is not a function");
+import React, { useState } from "react";
+import './styles.css'
+function ChipsInput() {
+  const [chipsInput, setChipsInput] = useState("")
+  const [allChips,setAllChips] = useState([])
+  const handleKey = (e) => {
+    if (chipsInput === '') {
+      return
+    }
+    if (e.key === 'Enter') {
+    e.preventDefault()
+      const filter = allChips.filter(item => item !== chipsInput)
+      setAllChips([...filter,chipsInput])
+      setChipsInput('')
+  }
   }
 
-  if (this.length === 0 && initialValue === undefined) {
-      throw new TypeError("Reduce of empty array with no initial value");
-  }
+  return (
+    <div className='main-container'>
+      <h2>Chips Input</h2>
+      <input
+        type="text" 
+        value={chipsInput}
+        onKeyDown={handleKey}
+        onChange={(e)=>setChipsInput(e.target.value)}
+        placeholder="Type a chip and press tag"
+        className="input"
+      />
+      <div style={{display:"flex", gap:"5px"}}>
+        {allChips.map(item => (
+          <p style={{
+            backgroundColor: "grey",
+            borderRadius: "2px",
+            padding:"5px"
+          }}>{item}</p>
+      ))}
+        </div>
+    </div>
+  );
+}
 
-  let accumulator = initialValue !== undefined ? initialValue : this[0];
-  let startIndex = initialValue !== undefined ? 0 : 1;
-
-  for (let i = startIndex; i < this.length; i++) {
-      if (Object.prototype.hasOwnProperty.call(this, i)) {
-          accumulator = callback(accumulator, this[i], i, this);
-      }
-  }
-
-  return accumulator;
-};
+export default ChipsInput;
